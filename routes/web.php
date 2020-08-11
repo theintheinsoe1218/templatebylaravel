9dee0e79ed5d0ded4249f3f84a7d8dbf092989db
theintheinsoe <theintheinsoe1218@gmail.com>
2020-08-11 20:43:01 +0630

    using template (route,controller,view)

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PageController@homefun')->name('home');  //getroute-- data show 
+
+Route::get('about','PageController@aboutfun')->name('aboutname');
+
+Route::get('contact','PageController@contactfun')->name('contactname');
+
+Route::get('samplepost','PageController@samplepostfun')->name('samplepost');
+
+
